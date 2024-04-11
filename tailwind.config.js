cd7ac8c60a9f72731354cbf2234edf6c38943c74
Awoyban <awoyban2@example.com>
2024-04-11 22:07:04 +0630

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "frontend/src/**/*.{js,ts,jsx,tsx}",
+    "client/index.html",
+    "client/src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
